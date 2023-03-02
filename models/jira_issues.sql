1dc9e8c086dda1907509fa5ab57abd6f575f835e
Tim Vergenz <vergenzt@gmail.com>
2023-03-01 23:01:14 -0500

    wip: got jira issues fetch to work!

--- models/jira_issues.sql
+++ models/jira_issues.sql
@@ -1,33 +1,31 @@
-{# with recursive
-  responses as ( #}
-    {%- set page_size = 50 %}
-    {%- set total_to_fetch = 1000 %}
-    {%- set lit = dbt.string_literal %}
+{%- if execute %}
+  {%- set pre_query -%}
+    select ({{ jira('GET', '/search', json_expr({ 'fields': '', 'maxResults': 0 })) }}->>'total')::int as total
+  {%- endset %}
+  {%- set total = run_query(pre_query).rows[0].total %}
+{%- else %}
+  {%- set total = 0 %}
+{%- endif %}
 
-    {{ config(pre_hook = "select http_set_curlopt('CURLOPT_TIMEOUT_MS', '20000')") }}
-
-    select
-      {{
-        jira('GET', '/search', json_expr({
-          'fields': lit('summary'),
-          'maxResults': page_size,
-          'startAt': 'start_at'
-        }))
-      }}
-    from
-      generate_series(0, {{ total_to_fetch }}, {{ page_size }}) as start_at
-{# 
-    union all
-
-    select
-      {{ jira('GET', '/search', json_expr({ 'startAt': jira_page_total('content') })) }}
-    from
-      responses
-    where
-      {{ jira_page_total('content') }} < {{ jira_total('content') }}
-  )
+{%- set lit = dbt.string_literal %}
+{%- set page_size = 50 %}
 
 select
-  *
+  issues_json.*
 from
-  responses #}
+  generate_series(0, {{ total - 1 }}, {{ page_size }}) as start_at,
+  json_to_recordset(
+    {{
+      jira('GET', '/search', json_expr({
+        'fields': lit('*all'),
+        'maxResults': page_size,
+        'startAt': 'start_at'
+      }))
+    }}
+    ->'issues'
+  )
+  as issues_json(
+    id text,
+    key text,
+    fields json
+  )
