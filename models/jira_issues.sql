5e987693c20e3fc2505f94c4cf0dfa51b2fd216c
Tim Vergenz <vergenzt@gmail.com>
2023-03-09 22:30:30 -0500

    wip

--- models/jira_issues.sql
+++ models/jira_issues.sql
@@ -13,19 +13,45 @@
 select
   issues_json.*
 from
-  generate_series(0, {{ total - 1 }}, {{ page_size }}) as start_at,
-  json_to_recordset(
-    {{
-      jira('GET', '/search', json_expr({
-        'fields': lit('*all'),
-        'maxResults': page_size,
-        'startAt': 'start_at'
-      }))
-    }}
-    ->'issues'
-  )
-  as issues_json(
-    id text,
-    key text,
-    fields json
-  )
+  generate_series(0, {{ total - 1 }}, {{ page_size }}) as start_at
+  cross join
+    json_to_recordset(
+      {{
+        jira('GET', '/search', json_expr({
+          'fields': lit('*all'),
+          'maxResults': page_size,
+          'startAt': 'start_at'
+        }))
+      }}
+      ->'issues'
+    )
+    as issues_json(
+      id text,
+      key text,
+      fields json
+    )
+
+{%- set insert_fn_name = this.replace(identifier=this.identifier ~ "_insert") %}
+{%- set insert_sql %}
+
+  create or replace function {{ insert_fn_name }}
+    returns trigger
+    language plpgsql
+    as $$
+    begin
+
+      {{
+        jira('GET', '/search', json_expr({
+          'fields': lit('*all'),
+          'maxResults': page_size,
+          'startAt': 'start_at'
+        }))
+      }}
+
+    end;
+    $$
+
+  create or replace trigger 
+    before insert on {{ this }}
+
+{%- endset %}
